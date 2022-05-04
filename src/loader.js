dfb77caae1b8ea81ab35776809513772f7d9a1df
Robin Somarajvasantha(UST <robin1985ad@gmail.com>
2022-05-03 21:41:05 -0500

    fixes and loader

---
+++ src/loader.js
@@ -0,0 +1,10 @@
+
+import logoLoader from './assets/images/logo-trimmed.png';
+export default function Loader() {
+    return (
+        <div className="loader-wrap">
+            <img src={logoLoader} />
+        </div>
+    )
+
+}
